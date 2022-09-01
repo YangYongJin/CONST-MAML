ae9ed79cb43ae4409a45b76693b35ca46d57715e
yongjinyang <dyyjkd@gmail.com>
2022-09-01 21:49:36 +0900

    initial commit

---
+++ train_maml_system.py
@@ -0,0 +1,24 @@
+from data import MetaLearningSystemDataLoader
+from experiment_builder import ExperimentBuilder
+from few_shot_learning_system import MAMLFewShotClassifier
+from utils.parser_utils import get_args
+from utils.dataset_tools import maybe_unzip_dataset
+import torch
+
+
+def run():
+    torch.multiprocessing.freeze_support()
+    # Combines the arguments, model, data and experiment builders to run an experiment
+    args, device = get_args()
+    model = MAMLFewShotClassifier(args=args, device=device,
+                                  im_shape=(2, args.image_channels,
+                                            args.image_height, args.image_width))
+    maybe_unzip_dataset(args=args)
+    data = MetaLearningSystemDataLoader
+    maml_system = ExperimentBuilder(
+        model=model, data=data, args=args, device=device)
+    maml_system.run_experiment()
+
+
+if __name__ == '__main__':
+    run()
